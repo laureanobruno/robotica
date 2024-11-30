cef7141aa3d831088b4b90742a02bfbb790627c0
Laureano Bruno <laureanobruno@alu.ing.unlp.edu.ar>
2024-11-30 20:00:49 -0300

    initial commit

---
+++ lau/template_ws/build/ros_gz_example_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/lau/template_ws/install/ros_gz_example_description/share/ros_gz_example_description/package.sh"
+. "/home/lau/template_ws/install/ros_gz_example_gazebo/share/ros_gz_example_gazebo/package.sh"
